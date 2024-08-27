dc0815f43f9e74f0387e5db41acff2d8420a28af
igregorioneto <igregoriodoneto@outlook.com>
2024-08-27 20:26:13 -0300

    feat: creating queries

--- library/execute-selects.sql
+++ library/execute-selects.sql
@@ -5,3 +5,28 @@ select m.name as "Name Member", m.email as "Email Member", b.title as "Title Boo
 from "Loans" l 
 join "Members" m on l.member_id = m.id
 join "Books" b on l.book_id = b.id;
+-- Conte o número total de livros emprestados atualmente.
+select COUNT(*) 
+from "Loans" l 
+join "Books" b on l.book_id = b.id;
+-- Liste todos os livros que ainda não foram devolvidos, juntamente com as informações do membro que pegou emprestado.
+select m."name" , b.title 
+from "Loans" l 
+join "Books" b ON l.book_id = b.id
+join "Members" m on l.member_id = m.id
+where l.return_date is null or l.return_date < '2024-04-10'
+group by m."name" , b.title ;
+-- Liste os membros que pegaram mais de 2 livros emprestados.
+select m.*
+from "Loans" l 
+join "Members" m on l.member_id = m.id
+group by m.id 
+having count(l.id) > 2;
+
+select *
+from "Members" m 
+where (
+	select count(*) 
+	from "Loans" l 
+	where l.member_id = m.id
+) > 2;
